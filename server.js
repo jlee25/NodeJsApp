4732da48aae7f72490637dca3a575367d0321519
Jeff <j_lee92@hotmail.com>
2019-01-09 21:52:13 -0500

    Add a Procfile

--- server.js
+++ server.js
@@ -11,5 +11,7 @@ app.use(express.static(__dirname + '/public'));
 
 controller(app);
 
-app.listen(3000);
-console.log('You are listening to port 3000');
+const server = app.listen(process.env.PORT || 5000, function() {
+  const port = server.address().port;
+  console.log("Express is working on port " + port);
+});
