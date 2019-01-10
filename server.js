71f2c290ce11458b98dd12c40beb961899223678
Jeff <j_lee92@hotmail.com>
2019-01-09 22:30:52 -0500

    highlighting

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const auth = require("./auth");
 
 app.use(auth);
 
+app.locals.highlight = () => {
+  var text = document.getElementById("query").value;
+  var query = new RegExp("(\\b" + text + "\\b)", "gim");
+  var e = document.getElementById("searchtext").innerHTML;
+  var enew = e.replace(/(<span>|<\/span>)/gim, "");
+  document.getElementById("searchtext").innerHTML = enew;
+  var newe = enew.replace(query, "<span>$1</span>");
+  document.getElementById("searchtext").innerHTML = newe;
+  console.log("hello");
+}
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
@@ -15,3 +26,5 @@ const server = app.listen(process.env.PORT || 5000, function() {
   const port = server.address().port;
   console.log("Express is working on port " + port);
 });
+
+app.listen(3000);
